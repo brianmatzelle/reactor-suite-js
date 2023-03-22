62560ab4fdb819018de291f539c58441b67cb506
brianmatzelle <bmatzel1@binghamton.edu>
2023-03-22 01:28:01 -0400

    half working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { VideoUploader } from './VideoUploader';
+import { VideoPlayer } from './VideoPlayer';
+import { DrawingCanvas } from './DrawingCanvas';
 
 function App() {
+  const [videoFile, setVideoFile] = useState(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <VideoUploader onUpload={setVideoFile} />
+      {videoFile && (
+        <>
+          <VideoPlayer videoFile={videoFile} />
+          <DrawingCanvas />
+        </>
+      )}
     </div>
   );
 }
