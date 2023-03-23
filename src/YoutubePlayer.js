da543d13f2522811e6ae82a79bc6ad420dd1793d
brianmatzelle <bmatzel1@binghamton.edu>
2023-03-23 03:32:46 -0400

    changed slow motion to arrow keys up/down

--- src/YoutubePlayer.js
+++ src/YoutubePlayer.js
@@ -52,22 +52,21 @@ export const YoutubePlayer = ({
         player.seekTo(player.getCurrentTime() + 5, true);
       } else if (e.code === 'ArrowLeft') {
         player.seekTo(player.getCurrentTime() - 5, true);
-      } else if (e.code === 'ArrowUp') {
-        player.setVolume(player.getVolume() + 10);
+      // } else if (e.code === 'ArrowUp') {
+      //   player.setVolume(player.getVolume() + 10);
+      // } else if (e.code === 'ArrowDown') {
+      //   player.setVolume(player.getVolume() - 10);
       } else if (e.code === 'ArrowDown') {
-        player.setVolume(player.getVolume() - 10);
+        player.seekTo(player.getCurrentTime() - 0.1, true); // Rewind by 100ms
+      } else if (e.code === 'ArrowUp') {
+        player.seekTo(player.getCurrentTime() + 0.1, true); // Fast-forward by 100ms
       } else if (e.code === 'KeyE') {
         toggleDrawing(); // Toggle drawing mode when "e" is pressed
       } else if (e.code === 'KeyC') {
         resetDrawing(); // Reset drawing when "r" is pressed
       } else if (e.code === 'KeyF') {
         toggleFullscreen(); // Toggle fullscreen when "f" is pressed
-      } else if (e.code === 'KeyK') {
-        player.seekTo(player.getCurrentTime() - 0.1, true); // Rewind by 100ms
-      } else if (e.code === 'KeyL') {
-        player.seekTo(player.getCurrentTime() + 0.1, true); // Fast-forward by 100ms
       }
-
       // Add any other keyboard controls you want to maintain when drawing is enabled
     };
 
